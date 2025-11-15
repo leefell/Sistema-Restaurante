9e2f1e8302143de91cb4f67a8242889da127793a
Alexandre <alexandreaug21@gmail.com>
2025-11-15 16:24:56 -0300

    refactor: remove a entidade garcom da api

--- api/src/app.js
+++ api/src/app.js
@@ -4,7 +4,6 @@ const dotenv = require('dotenv');
 const app = express();
 
 const usuarioRoutes = require('./routes/usuario.routes.js');
-const garcomRoutes = require('./routes/garcom.routes.js');
 
 dotenv.config();
 
@@ -12,6 +11,5 @@ app.use(express.json());
 app.use(cors());
 
 app.use('/usuarios', usuarioRoutes);
-app.use('/garcom', garcomRoutes);
 
 module.exports = app;
